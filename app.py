401ce935f010465d0bc089f01b9d6cc91c54bbaa
gustavosantanavs-jpg <gustavosantanavs@gmail.com>
2026-03-05 19:29:52 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Gerador de Provas", page_icon="📝")
+
+st.title("📚 Gerador de Provas Anti-Cola - Profa. Milena")
+st.write("O sistema está online! O nosso próximo passo será adicionar a inteligência para ler e embaralhar os arquivos do Word.")
